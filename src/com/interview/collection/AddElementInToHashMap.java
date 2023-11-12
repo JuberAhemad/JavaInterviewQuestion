3e56817c60d253c5ca6370096dd1aa985df67776
JuberAhemad <juber.ahemad@yahoo.com>
2023-11-12 21:08:06 +0530

    added hshmap class

---
+++ src/com/interview/collection/AddElementInToHashMap.java
@@ -0,0 +1,21 @@
+package com.interview.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *Write a program to add elements to the HashMap given the key and value data type is String? 
+ **/
+public class AddElementInToHashMap {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("name", "Juber Ahemad");
+		map.put("email", "juber@gmail.com");
+		map.put("dept", "IT");
+		map.put("phone", "9555910872");
+		
+	}
+
+}
